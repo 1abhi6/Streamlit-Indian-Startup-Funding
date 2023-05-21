225713b01d2e585b9cb16b59534f2061bc2afd79
1abhi6 <1abhigup6@gmail.com>
2023-05-22 02:35:59 +0530

    Write basic skeleton code

--- app.py
+++ app.py
@@ -9,4 +9,23 @@ import streamlit as st
 import pandas as pd
 import matplotlib as plt
 
-st.title('Overall Analysis')
+
+
+df = pd.read_csv('dataset/startup_funding.csv')
+
+# data cleaning
+df['Investors Name'] = df['Investors Name'].fillna('Undisclosed')
+
+st.sidebar.title('Startup Funding analysis')
+option = st.sidebar.selectbox('Select One',['Overall Analysis','Startup','Investor'])
+
+if option == 'Overall Analysis':
+    st.title('Overall Analysis')
+elif option == 'Startup':
+    st.sidebar.selectbox('Select Startup',sorted(df['Startup Name'].unique().tolist()))
+    btn1 = st.sidebar.button('Find Startup details')
+    st.title('Startup Analysis')
+elif option == 'Investor':
+    st.sidebar.selectbox('Select Startup',sorted(df['Investors Name'].unique().tolist()))
+    btn1 = st.sidebar.button('Find Investor details')
+    st.title('Investor Analysis')
