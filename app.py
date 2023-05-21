8bc56f2f8cd33f0f8f2148c8d62a936d1d5af2fa
1abhi6 <1abhigup6@gmail.com>
2023-05-21 23:31:41 +0530

    Some imporovements

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+"""Streamlit App
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
 import streamlit as st
 import pandas as pd
 import matplotlib as plt
-
 
 st.title('Overall Analysis')
