3f03928e6f2113c4992a9351b59eef62aef6e15b
1abhi6 <1abhigup6@gmail.com>
2023-05-22 23:46:22 +0530

    Add OOPS in app.py

--- app.py
+++ app.py
@@ -4,28 +4,31 @@ argument -- description
 Return: return_description
 """
 
-
 import streamlit as st
 
 from analysis import Investor as InvestorAnalysis
-from analysis import Startup as StartupAnalysis
 from components import Investor as InvestorComponent
 
+class Main:
+    def __init__(self) -> None:
+        self.investor_analysis = InvestorAnalysis()
+        self.investor_component = InvestorComponent()
+        self.home_component()
 
-investor_analysis = InvestorAnalysis()
-investor_component = InvestorComponent()
-st.sidebar.title('Startup Funding analysis')
-option = st.sidebar.selectbox('Select One',['Overall Analysis','Startup','Investor'])
+    def home_component(self):
+        st.sidebar.title('Startup Funding analysis')
+        option = st.sidebar.selectbox('Select One',['Overall Analysis','Startup','Investor'])
 
-if option == 'Overall Analysis':
-    st.title('Overall Analysis')
-elif option == 'Startup':
-    st.sidebar.selectbox('Select Startup',['Ola','Unacademy'])
-    btn1 = st.sidebar.button('Find Startup details')
-    st.title('Startup Analysis')
-elif option == 'Investor':
-    investor_name = st.sidebar.selectbox('Select Startup',investor_analysis.investor_list())
-    btn1 = st.sidebar.button('Find Investor details')
-    st.title('Investor Detail')
-    if btn1:
-        investor_component.five_most_recent_investments(investor_name)
+        if option == 'Overall Analysis':
+            st.title('Overall Analysis')
+        elif option == 'Startup':
+            st.sidebar.selectbox('Select Startup',['Ola','Unacademy'])
+            btn1 = st.sidebar.button('Find Startup details')
+            st.title('Startup Analysis')
+        elif option == 'Investor':
+            investor_name = st.sidebar.selectbox('Select Startup',self.investor_analysis.investor_list())
+            btn1 = st.sidebar.button('Find Investor details')
+            st.title('Investor Detail')
+            if btn1:
+                self.investor_component.five_most_recent_investments(investor_name)
+Main()
