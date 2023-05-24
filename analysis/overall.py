d7209f862922eb4cda7697df68fef6a55a19df37
1abhi6 <1abhigup6@gmail.com>
2023-05-25 03:34:57 +0530

    Add analysis for Most funded sector

--- analysis/overall.py
+++ analysis/overall.py
@@ -9,17 +9,16 @@ class Overall:
 
     def total_invested_amount(self):
         return round(self.startup['amount'].sum())
-    
+
     def max_amount_infused(self):
         return self.startup.groupby('name')['amount'].max().sort_values(ascending=False).head(1).values[0]
-    
+
     def avg_ticket_size(self):
         return self.startup.groupby('name')['amount'].sum().mean()
-    
+
     def total_funded_startup(self):
         return self.startup['name'].nunique()
-    
-    
+
     def total_funding_mom(self):
         temp_df = startup.groupby(['year','month'])['amount'].sum().reset_index()
         temp_df['MM-YYYY'] = temp_df['month'].astype('str') + '-' + temp_df['year'].astype('str')
@@ -28,7 +27,7 @@ class Overall:
         },inplace=True)
 
         return temp_df
-    
+
     def total_funded_startup_mom(self):
         temp_df = startup.groupby(['year','month'])['amount'].count().reset_index()
         temp_df['MM-YYYY'] = temp_df['month'].astype('str') + '-' + temp_df['year'].astype('str')
@@ -37,3 +36,12 @@ class Overall:
         },inplace=True)
 
         return temp_df
+    
+    def most_funded_sector(self):
+        temp_df = startup.groupby('vertical')['amount'].sum().reset_index()
+        most_funded_sectors = temp_df[temp_df['amount'] != 0.0].sort_values(by='amount',ascending=False).head(10)
+        most_funded_sectors['amount'] = round(most_funded_sectors['amount'],2)
+
+        return most_funded_sectors
+    
+    
