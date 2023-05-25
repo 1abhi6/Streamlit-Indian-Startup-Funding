f353725c479c7fa8e53a53ac48ceff81ac7b6223
1abhi6 <1abhigup6@gmail.com>
2023-05-25 15:00:52 +0530

    Similar Startup analysis

--- analysis/startup.py
+++ analysis/startup.py
@@ -24,3 +24,15 @@ class Startup:
     
     def investment_date(self,startup_name):
         return self.startup[self.startup['name'] == startup_name]['date'].values[0]
+    
+    def similar_startups(self,startup_name):
+        vertical = startup.loc[startup['name'] == startup_name, 'vertical'].values[0]
+
+        temp_startups = startup.loc[startup['vertical'] == vertical, 'name'].unique()
+
+        similar_startups = []
+        for company in temp_startups:
+            if company != startup_name:
+                similar_startups.append(company)
+        
+        return similar_startups
