89d35a29a583cd57a12395bdb99fba60a708cd17
1abhi6 <1abhigup6@gmail.com>
2023-05-25 14:50:48 +0530

    Startup sector analysis

--- analysis/startup.py
+++ analysis/startup.py
@@ -6,3 +6,8 @@ from dataset import startup
 class Startup:
     def __init__(self):
         self.startup = startup
+
+    def sector(self):
+        return self.startup[self.startup['name'] == 'Mamaearth']['vertical'].values[0]
+    
+    
